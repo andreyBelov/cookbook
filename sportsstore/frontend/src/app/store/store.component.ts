7091f6783cb4f97a296d5a2177743c05bdf44d1a
aebeloborodov <abeloborodov@ppr.ru>
2019-12-08 19:35:26 +0300

    sportsstore frontend: added pagination on store component

--- sportsstore/frontend/src/app/store/store.component.ts
+++ sportsstore/frontend/src/app/store/store.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {ProductRepository} from "../model/product.repository";
 import {Product} from "../model/product.model";
 
@@ -10,11 +10,16 @@ import {Product} from "../model/product.model";
 export class StoreComponent {
 
   public selectedCategory = null;
+  public productsPerPage = 4;
+  public selectedPage = 1;
 
-  constructor(private repository: ProductRepository) { }
+  constructor(private repository: ProductRepository) {
+  }
 
   get products(): Product[] {
-    return this.repository.getProducts(this.selectedCategory);
+    let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+    return this.repository.getProducts(this.selectedCategory)
+      .slice(pageIndex, pageIndex + this.productsPerPage);
   }
 
   get categories(): string[] {
@@ -24,4 +29,24 @@ export class StoreComponent {
   changeCategory(newCategory?: string) {
     this.selectedCategory = newCategory;
   }
+
+  changePage(newPage: number) {
+    this.selectedPage = newPage;
+  }
+
+  changePageSize(newSize: number) {
+    this.productsPerPage = Number(newSize);
+    this.changePage(1);
+  }
+
+  // get pageNumbers(): number[] {
+  //   let productCount = this.repository.getProducts(this.selectedCategory).length;
+  //   return Array(Math.ceil(productCount / this.productsPerPage)).fill(0).map((item, ind) => ind + 1);
+  // }
+
+  get pageCount(): number {
+    let productCount = this.repository.getProducts(this.selectedCategory).length;
+    return Math.ceil(productCount / this.productsPerPage)
+  }
+
 }
